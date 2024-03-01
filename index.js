aaa344d25b8b630ecebf2b6a7c9eda767cb62668
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2024-03-01 23:30:40 +0530

    link update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: 'https://pro-manage-frontend-brown.vercel.app/login' }));
 app.use('/api/', userRoute);
 app.use('/api/', taskRoute);
 
